ed89d892cad297fc85dce4a8eac09f910d7128cc
No EMILIEN <No EMILIEN@DESKTOP-AQT6DK5>
2024-10-23 15:02:52 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿/**
+ * Feu routier : utilisation char (à 3 valeurs)
+ * Author : EMILIEN NOAM
+ * Date : 22/10/2024
+ */
+
+using System;
+
+namespace testEnterprise2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char feu;
+
+            do
+            {
+                Console.Write("Couleur du feu (R/O/V) = ");
+                feu = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+            } while (feu != 'R' && feu != 'r' && feu != 'O' && feu != 'o' && feu != 'V' && feu != 'v');
+
+            if (feu == 'R' || feu == 'r')
+            {
+                Console.WriteLine("S'arrêter");
+            }
+            else if (feu == 'O' || feu == 'o') 
+            {
+                Console.WriteLine("Ralentir");
+            }
+            else  
+            {
+                Console.WriteLine("Avancer");
+            }
+            Console.ReadLine();
+        }
+    }
+}
